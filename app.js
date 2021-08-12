151026124725ab9ae096dea500af96c956b4655e
smocky <dsmock@gmail.com>
2021-08-12 11:08:11 -0400

    filter out the person calling the command

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config();
-const { App, WorkflowStep } = require('@slack/bolt');
+const { App } = require('@slack/bolt');
 
 // Initializes your app in socket mode with your app token and signing secret
 const app = new App({
@@ -10,78 +10,16 @@ const app = new App({
     appToken: process.env.SLACK_APP_TOKEN // add this
 });
 
-// Create a new WorkflowStep instance
-const ws = new WorkflowStep('get_random_member', {
-    edit: async ({ ack, step, configure }) => {
-        await ack();
-
-        const blocks = [
-            {
-                type: "input",
-                block_id: "channel",
-                element: {
-                    type: "conversations_select",
-                    action_id: "channel",
-                    placeholder: {
-                        type: "plain_text",
-                        text: "Select the channel"
-                    }
-                },
-                label: {
-                    type: "plain_text",
-                    text: "Channel"
-                }
-            }
-        ];
-        await configure({ blocks });
-
-    },
-    save: async ({ ack, step, view, update }) => {
-        await ack();
-        const { values } = view.state;
-        const channel = values.channel.channel;
-        const inputs = {
-            channel: { value: channel.selected_conversation }
-        };
-        const outputs = [
-            {
-                type: 'user',
-                name: 'randoUser',
-                label: 'Random User',
-            }
-        ]
-        await update({ inputs, outputs });
-    },
-    execute: async ({ body, step, complete, fail }) => {
-        const { inputs } = step;
-
-        const result = await app.client.conversations.members({
-            channel: inputs.channel.value
-        });
-        const randomUser = result.members[Math.floor(Math.random() * result.members.length)];
-
-        const outputs = {
-            randoUser: randomUser,
-        };
-
-        // if everything was successful
-        await complete({ outputs });
-
-        // if something went wrong
-        // fail({ error: { message: "Just testing step failure!" } });
-    },
-});
-
-app.step(ws);
-
-// Shortcut to kick this off
+// Command to kick this off
 app.command('/rando', async ({ ack, command, client }) => {
     try {
         await ack();
         const result = await client.conversations.members({
             channel: command.channel_id
         });
-        const randomUser = result.members[Math.floor(Math.random() * result.members.length)];
+        // Filter out the person who called the command
+        let members = result.members.filter(user => user != command.user_id);
+        const randomUser = members[Math.floor(Math.random() * members.length)];
         client.chat.postMessage({
             channel: command.channel_id,
             text: "Congrats <@" + randomUser + "> :tada: !!"
